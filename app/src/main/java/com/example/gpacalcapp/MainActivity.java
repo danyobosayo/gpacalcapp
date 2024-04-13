dcff175c13121eb3d4573dd20016b197c4d62612
danyobosayo <sungsu.kim04@gmail.com>
2024-04-13 00:43:24 -0500

    Created new elements for homepage. Created an incomplete Button Press listen function.

--- app/src/main/java/com/example/gpacalcapp/MainActivity.java
+++ app/src/main/java/com/example/gpacalcapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.gpacalcapp;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -9,6 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,11 +18,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+    }
+    public void calculate(View v) {
+        // call the method.
 
-            return insets;
-        });
+
     }
 }
